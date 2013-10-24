f1f6a4e06f53e2996183d1a9de176f8e39927b09
Keith Duncan <keith.duncan@github.com>
2013-10-24 14:03:09 +0100

    Add admin interface views

--- app/site.rb
+++ app/site.rb
@@ -1,12 +1,24 @@
+require 'mustache'
+
 module Squirrel
   class Site < Sinatra::Base
 
-    get '/latest' do
+    register Mustache::Sinatra
+    set :mustache, {
+      :namespace => Squirrel,
+      :templates => File.join(File.dirname(__FILE__), "templates"),
+      :views => File.join(File.dirname(__FILE__), "views"),
+    }
 
+    get '/latest' do
+      @latest_release = Release.latest_release
+      mustache :releases
     end
 
     post '/latest' do
 
+
+      redirect '/latest'
     end
 
   end
---
+++ app/views/releases.rb
@@ -0,0 +1,11 @@
+module Squirrel
+  module Views
+    class Releases < Mustache
+
+      def latest_release
+        @latest_release
+      end
+
+    end
+  end
+end
