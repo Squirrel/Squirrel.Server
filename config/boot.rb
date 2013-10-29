68058e56b827d49b46e09c38a0d7e37e63a98e3e
Keith Duncan <keith.duncan@github.com>
2013-10-29 12:59:42 +0000

    Don’t need the check for nil RACK_ENV, a default is provided

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,7 @@
 require 'bundler'
 Bundler.require(:default)
 
-environment_name = ENV['RACK_ENV'] ||= 'development'
-if environment_name
-  Bundler.require(environment_name.to_sym)
-end
-
 ENV['RACK_ROOT'] ||= File.join(File.dirname(__FILE__), "..")
+
+environment_name = ENV['RACK_ENV'] ||= 'development'
+Bundler.require(environment_name.to_sym)
